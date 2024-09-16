69cac2b5d121680cdabe36048aa76330f15a956a
Bjørn Olav <bo@jalborg.com>
2024-09-16 10:21:09 +0200

    drivechain: make createsidechaindeposit work
    accomplish this by changing the type of the command from btcutil.Amount to float64.

--- server/commands/drivechain.go
+++ server/commands/drivechain.go
@@ -1,10 +1,8 @@
 package commands
 
-import "github.com/btcsuite/btcd/btcutil"
-
 type CreateSidechainDeposit struct {
-	Slot        int            `json:"slot"`
-	Destination string         `json:"destination"`
-	Amount      btcutil.Amount `json:"amount"`
-	Fee         btcutil.Amount `json:"fee"`
+	Slot        int     `json:"slot"`
+	Destination string  `json:"destination"`
+	Amount      float64 `json:"amount"`
+	Fee         float64 `json:"fee"`
 }
--- server/drivechain_server.go
+++ server/drivechain_server.go
@@ -20,11 +20,6 @@ import (
 var _ rpc.DrivechainServiceHandler = new(Bitcoind)
 
 func (b *Bitcoind) CreateSidechainDeposit(ctx context.Context, c *connect.Request[pb.CreateSidechainDepositRequest]) (*connect.Response[pb.CreateSidechainDepositResponse], error) {
-	type rawCreateResponse struct {
-		TXID   string   `json:"txid"`
-		Errors []string // May be empty
-	}
-
 	amount, err := btcutil.NewAmount(c.Msg.Amount)
 	if err != nil {
 		return nil, fmt.Errorf("amount: %w", err)
@@ -43,32 +38,31 @@ func (b *Bitcoind) CreateSidechainDeposit(ctx context.Context, c *connect.Reques
 	withoutS := strings.TrimPrefix(c.Msg.Destination, "s")
 	sidechainSlot := strings.Split(withoutS, "_")[0]
 
-	return withCancel[rawCreateResponse, pb.CreateSidechainDepositResponse](ctx,
-		func(ctx context.Context) (rawCreateResponse, error) {
+	return withCancel(ctx,
+		func(ctx context.Context) (string, error) {
 			cmd, err := btcjson.NewCmd("createsidechaindeposit", sidechainSlot, c.Msg.Destination, amount.ToBTC(), fee.ToBTC())
 			if err != nil {
-				return rawCreateResponse{}, err
+				return "", fmt.Errorf("createsidechaindeposit new cmd: %w", err)
 			}
 
 			res, err := rpcclient.ReceiveFuture(b.rpc.SendCmd(ctx, cmd))
 			if err != nil {
-				return rawCreateResponse{}, fmt.Errorf("send createsidechaindeposit: %w", err)
+				return "", fmt.Errorf("createsidechaindeposit send: %w", err)
 			}
 			zerolog.Ctx(ctx).Err(err).
 				Msgf("createsidechaindeposit response: %s", string(res))
 
-			var parsed rawCreateResponse
-			if err := json.Unmarshal(res, &parsed); err != nil {
-				return rawCreateResponse{}, fmt.Errorf("unmarshal createsidechaindeposit response: %w", err)
+			var txid string
+			if err := json.Unmarshal(res, &txid); err != nil {
+				return "", fmt.Errorf("createsidechaindeposit unmarshal response: %w", err)
 			}
 
-			return parsed, nil
+			return txid, nil
 		},
 
-		func(r rawCreateResponse) *pb.CreateSidechainDepositResponse {
+		func(txid string) *pb.CreateSidechainDepositResponse {
 			return &pb.CreateSidechainDepositResponse{
-				Txid:   r.TXID,
-				Errors: r.Errors,
+				Txid: txid,
 			}
 		},
 	)
