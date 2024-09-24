999a9636f977bf757042c622d64e28e2c5551fd2
Bjørn Olav <bo@jalborg.com>
2024-09-24 10:31:05 +0200

    rpc: add listactivesidechains

--- server/drivechain_server.go
+++ server/drivechain_server.go
@@ -67,3 +67,130 @@ func (b *Bitcoind) CreateSidechainDeposit(ctx context.Context, c *connect.Reques
 		},
 	)
 }
+
+type activeSidechain struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	NVersion    int    `json:"nversion"`
+	HashID1     string `json:"hashid1"`
+	HashID2     string `json:"hashid2"`
+}
+
+// ListActiveSidechains implements drivechaindv1connect.DrivechainServiceHandler.
+func (b *Bitcoind) ListActiveSidechains(ctx context.Context, _ *connect.Request[pb.ListActiveSidechainsRequest]) (*connect.Response[pb.ListActiveSidechainsResponse], error) {
+	return withCancel(ctx, func(ctx context.Context) ([]*pb.ListActiveSidechainsResponse_Sidechain, error) {
+		cmd, err := btcjson.NewCmd("listactivesidechains")
+		if err != nil {
+			return nil, fmt.Errorf("listactivesidechains new cmd: %w", err)
+		}
+
+		res, err := rpcclient.ReceiveFuture(b.rpc.SendCmd(ctx, cmd))
+		if err != nil {
+			return nil, fmt.Errorf("listactivesidechains send: %w", err)
+		}
+
+		var sidechains []activeSidechain
+		if err := json.Unmarshal(res, &sidechains); err != nil {
+			return nil, fmt.Errorf("listactivesidechains unmarshal response: %w", err)
+		}
+
+		// Log the unmarshaled sidechains for debugging
+		zerolog.Ctx(ctx).Debug().
+			Interface("sidechains", sidechains).
+			Msg("unmarshaled active sidechains")
+
+		pbSidechains := make([]*pb.ListActiveSidechainsResponse_Sidechain, 0, len(sidechains))
+		for _, sidechain := range sidechains {
+			pbSidechain, err := b.sidechainToProto(ctx, sidechain)
+			if err != nil {
+				return nil, fmt.Errorf("convert sidechain to proto: %w", err)
+			}
+
+			pbSidechains = append(pbSidechains, pbSidechain)
+		}
+
+		return pbSidechains, nil
+	},
+
+		func(sidechains []*pb.ListActiveSidechainsResponse_Sidechain) *pb.ListActiveSidechainsResponse {
+			return &pb.ListActiveSidechainsResponse{
+				Sidechains: sidechains,
+			}
+		},
+	)
+}
+
+func (b *Bitcoind) getChaintipInfo(ctx context.Context, sidechainSlot int) (btcutil.Amount, string, error) {
+	cmd, err := btcjson.NewCmd("listsidechainctip", sidechainSlot)
+	if err != nil {
+		return 0, "", fmt.Errorf("listsidechainctip new cmd: %w", err)
+	}
+	res, err := rpcclient.ReceiveFuture(b.rpc.SendCmd(ctx, cmd))
+	if err != nil {
+		return 0, "", fmt.Errorf("listsidechainctip send: %w", err)
+	}
+
+	var result struct {
+		Amount btcutil.Amount `json:"amount"`
+		TXID   string         `json:"txid"`
+	}
+	if err := json.Unmarshal(res, &result); err != nil {
+		return 0, "", fmt.Errorf("unmarshal listsidechainctip response: %w", err)
+	}
+
+	return result.Amount, result.TXID, nil
+}
+
+func (b *Bitcoind) sidechainToProto(
+	ctx context.Context, sidechain activeSidechain,
+) (*pb.ListActiveSidechainsResponse_Sidechain, error) {
+	slot, err := chainToSlot(sidechain.Title)
+	if err != nil {
+		return nil, fmt.Errorf("convert chain to slot: %w", err)
+	}
+
+	balance, txid, err := b.getChaintipInfo(ctx, slot)
+	switch {
+	case err == nil:
+		// No error, proceed
+
+	case strings.Contains(err.Error(), drivechain.ErrNoCTip):
+		zerolog.Ctx(ctx).Warn().Int("slot", slot).Msg("no ctip for sidechain")
+		// That's okay, proceed
+
+	default:
+		return nil, fmt.Errorf("get balance for chain %s: %w", sidechain.Title, err)
+	}
+
+	return &pb.ListActiveSidechainsResponse_Sidechain{
+		Title:         sidechain.Title,
+		Description:   sidechain.Description,
+		Nversion:      uint32(sidechain.NVersion),
+		Hashid1:       sidechain.HashID1,
+		Hashid2:       sidechain.HashID2,
+		Slot:          int32(slot),
+		AmountSatoshi: int64(balance),
+		ChaintipTxid:  txid,
+	}, nil
+}
+
+func chainToSlot(chain string) (int, error) {
+	switch chain {
+	case "Testchain":
+		return 0, nil
+	case "BitNames":
+		return 2, nil
+	case "BitAssets":
+		return 4, nil
+	case "ZSide":
+		return 5, nil
+	case "EthSide":
+		return 6, nil
+	case "Thunder":
+		return 9, nil
+	case "LatestCore":
+		return 11, nil
+	default:
+		return 0, fmt.Errorf("unknown chain: %s", chain)
+	}
+}
